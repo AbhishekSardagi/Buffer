db9825d1a50738a54f406f2f7f58208d56c6b5b4
meera <shu@ahb.com>
2021-03-13 10:21:17 +0530

    Buffer

--- Javascript/login.js
+++ Javascript/login.js
@@ -5,7 +5,7 @@ function check() {
     var x = document.getElementById("CE").value
     var y = document.getElementById("CP").value
     if (email1 == x && password1 == y) {
-        window.location.href = "/Users/kaustubhmendhe/Documents/unit2/project/buffer/Buffer/HTML/loggedin.html"
+        window.location.href = "/Users/kaustubhmendhe/Documents/unit2/project/buffer/Buffer/HTML/1.html"
     }
     else {
         alert("Wrong Password or Email")
